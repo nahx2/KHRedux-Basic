ff81d10256030c7e456aa274ea306df3e1669509
nahx2 <108976269+nahx2@users.noreply.github.com>
2022-10-20 02:49:44 +0900

    Update TomatoTalk.jsx

--- src/components/talk/TomatoTalk.jsx
+++ src/components/talk/TomatoTalk.jsx
@@ -5,17 +5,7 @@ import { Button, Form, InputGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MessageLi } from "../style/TalkStyle";
 
-/* const firebaseConfig = {
-  apiKey: "AIzaSyCIS_v1-BdrTwGF2BQrGgPoiDd2u1A6XZw",
-  authDomain: "virtual-primer-354906.firebaseapp.com",
-  databaseURL:
-    "https://virtual-primer-354906-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "virtual-primer-354906",
-  storageBucket: "virtual-primer-354906.appspot.com",
-  messagingSenderId: "442922054622",
-  appId: "1:442922054622:web:aaa9cf4f99a2a45ef8a34f",
-  measurementId: "G-DM03L94YWH",
-}; */
+
 
 //const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase();
